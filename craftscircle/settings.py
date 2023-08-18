aee4c8c2157b7b8d305ee5cf4a732b66ec37f548
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-08-18 16:13:43 +0000

    Initial heroku deploy

--- craftscircle/settings.py
+++ craftscircle/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ahmadalmasr-craftscircl-w2ag7kdatmv.ws-eu104.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8000-ahmadalmasr-craftscircl-w2ag7kdatmv.ws-eu104.gitpod.io', 'localhost', 'mb2test.herokuapp.com']
 
 
 # Application definition
