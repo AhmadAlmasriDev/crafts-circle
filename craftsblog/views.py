334d69b1e9ac5c2ebd2a5db99c18d3bcca3924ee
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-08-22 11:40:04 +0000

    Add infinate scroll to post list htmx

--- craftsblog/views.py
+++ craftsblog/views.py
@@ -7,14 +7,11 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     context_object_name = "post_list"
-    template_name = "index.html"
+    # template_name = "index.html"
     paginate_by = 8
 
     def get_template_names(self):
         if self.request.htmx:
-            print("xxxxx")
-            return "test.html"
-        # print(self.request.htmx)
-        print(self.request.htmx)
+            return "partials/post_list_items.html"
         return "index.html"
 
